df2d67a83e40a9ba91fa7a28094e4108c86063e8
Duc Cuong <duccuong.hust@gmail.com>
2016-08-01 10:08:14 +0700

    Translate language

--- forms.py
+++ forms.py
@@ -7,16 +7,17 @@ sys.setdefaultencoding("utf-8")
 
 
 class ParcelRequestForm(Form):
-    parcel_type = SelectField('Loại hàng hóa', choices=[(
-        'Đồ điện tử', 'Đồ điện tử'), ('Đồ gia dụng', 'Đồ gia dụng'),
-        ('Hải sản', 'Hải sản'), ('Rau củ quả', 'Rau củ quả'),
-        ('Bưu kiện', 'Bưu kiện')])
-    parcel_weight = StringField('Khối lượng')
-    parcel_dimension = StringField('Kích thước')
-    parcel_arrive_time = SelectField('Thời gian nhận', choices=[
+    parcel_type = SelectField('Parcel type', choices=[(
+        'Electronics', 'Electronics'),
+        ('Household appliances', 'Household appliances'),
+        ('Seafood', 'Seafood'), ('Food and Beverage', 'Food and Beverage'),
+        ('Express package', 'Express package')])
+    parcel_weight = StringField('Weight')
+    parcel_dimension = StringField('Size')
+    parcel_arrive_time = SelectField('Received time', choices=[
         ('12h đến 2h', '12h đến 2h'), ('2h đến 4h', '2h đến 4h'),
         ('4h đến 6h', '4h đến 6h'), ('6h đến 8h', '6h đến 8h'),
         ('8h đến 12h', '8h đến 12h')])
-    parcel_fee = DecimalField('Tiền thu hộ')
-    parcel_note = StringField('Ghi chú')
+    parcel_fee = DecimalField('Package fee')
+    parcel_note = StringField('Note')
     parcel_place = HiddenField('parcel_place')
